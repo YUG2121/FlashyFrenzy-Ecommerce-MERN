09816701cf516988541fc36e7b184884a3944c69
Yug-upadhyay <yugnum1@gmail.com>
2025-09-09 16:45:00 +0530

    reuploded

---
+++ FrontEnd/src/pages/Verify.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect } from 'react'
+import { ShopContext } from '../context/ShopContex'
+import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
+
+import {toast} from 'react-toastify'
+import { backendUrl } from '../App'
+const Verify = () => {
+
+    const{navigate,token,setCartItems} = useContext(ShopContext)
+    const [ searchParams,setSearchParams] = useSearchParams()
+    const success = searchParams.get('success')
+    const orderId = searchParams.get('orderId')
+    const verifyPayment  = async (params) => {
+        try {
+            if (!token) {
+                return null
+            }
+            const response =  await axios.post(backendUrl+'/api/order/verifystripe' ,{success,orderId},{headers:{token}})
+           if (response.data.success) {
+             setCartItems({})
+             navigate('/orders')
+           }else{
+            navigate('/cart')
+           }
+           
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message
+            )
+            
+        }
+    }
+
+    useEffect(()=>{
+verifyPayment()
+    },[token])
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Verify
