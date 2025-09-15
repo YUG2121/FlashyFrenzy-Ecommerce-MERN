b25de161d702a3a79d00b022a2153d65fa0bf3d7
Yug Upadhyay <114340240+YUG2121@users.noreply.github.com>
2025-09-15 13:40:29 +0530

    Update navbar.jsx

--- FrontEnd/src/componet/navbar.jsx
+++ FrontEnd/src/componet/navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
  }  
   return (
     <div className=" flex items-center justify-between py-5  font-medium">
-    <Link to='/'>  <img src={assets.logo} className="w-36 mr-5 " alt="" /></Link>
+    <Link to='/'>  <img src={assets.logo} className="w-[70px] mr-5 " alt="" /></Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1 mx-2">
           <p className="text-[#1C352D]">HOME</p>
