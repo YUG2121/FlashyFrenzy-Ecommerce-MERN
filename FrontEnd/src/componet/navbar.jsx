259e3da5fdc8b493904c87dd413112b79a5c5352
Yug Upadhyay <114340240+YUG2121@users.noreply.github.com>
2025-09-15 13:47:53 +0530

    Update navbar.jsx

--- FrontEnd/src/componet/navbar.jsx
+++ FrontEnd/src/componet/navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   setCartItems({});
  }  
   return (
-    <div className=" flex items-center justify-between py-5  font-medium">
+    <div className=" flex items-center justify-between   font-medium">
     <Link to='/'>  <img src={assets.logo} className="w-[70px] mr-5 " alt="" /></Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1 mx-2">
